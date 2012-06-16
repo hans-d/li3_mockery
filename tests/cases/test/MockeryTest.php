8cf7b136e29c2efe0a1a053019fbf34a4d90ddd4
Hans Donner <hans.donner@pobox.com>
2012-06-16 16:40:20 +0200

    some testcases

--- tests/cases/test/MockeryTest.php
+++ tests/cases/test/MockeryTest.php
@@ -1,48 +1,28 @@
 <?php
 
-namespace li3_mockery\tests\cases\test;
+namespace app\tests\cases\test;
 
 use li3_mockery\test\Mockery as m;
 
-
-/**
- * Run the sample as found on https://github.com/padraic/mockery
- *
- */
 class MockeryTest extends \lithium\test\Unit {
 
 	public function tearDown() {
 		m::close();
 	}
 
-	public function testGetsAverageTemperatureFromThreeServiceReadings()
-	{
-		$service = m::mock('service');
-		$service->shouldReceive('readTemp')->times(3)->andReturn(10, 12, 14);
+	public function testMock() {
+		$mock = m::mock('test');
+		$mock->shouldReceive('go')->twice()->andReturn(1);
 
-		$temperature = new Temperature($service);
-		$this->assertEqual(12, $temperature->average());
-	}
-}
+		$result = $mock->go();
+		$expected = 2;
+		$this->assertEqual($expected, $result);
 
-/**
- * Sample class to mock, as found on https://github.com/padraic/mockery
- *
- */
-class Temperature {
-
-	public function __construct($service) {
-		$this->_service = $service;
 	}
 
-	public function average() {
-		$total = 0;
-		for ($i=0; $i<3; $i++) {
-			$total += $this->_service->readTemp();
-		}
-		return $total/3;
+	public function testSimple() {
+		$this->assertEqual(1, 2);
 	}
+
 
 }
-
-?>
